5979ee3a50daed59f10271ff051fc5c0b5ec0304
Ajon Subedi <ajonsubedi@gmail.com>
2018-01-08 18:17:57 +0100

    first commit

---
+++ app/src/main/java/com/example/ajonsubedi/walkarm/MyAlarmAdapter.java
@@ -0,0 +1,20 @@
+package com.example.ajonsubedi.walkarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Ajon Subedi on 1/8/2018.
+ */
+
+public class MyAlarmAdapter extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
