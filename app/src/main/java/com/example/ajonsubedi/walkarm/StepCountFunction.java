f1597f836441f59876627a63f7b77ea662aa43a2
Feng Lin <lin.feng55@hotmail.com>
2018-01-09 23:00:07 +0100

    functies toegevoegd bij stepCountFunction

--- app/src/main/java/com/example/ajonsubedi/walkarm/StepCountFunction.java
+++ app/src/main/java/com/example/ajonsubedi/walkarm/StepCountFunction.java
@@ -2,10 +2,17 @@ package com.example.ajonsubedi.walkarm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.Messenger;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by fenglin on 8/01/18.
@@ -13,37 +20,86 @@ import android.os.Messenger;
 
 public class StepCountFunction {
 
-    int aantalStappen;
+    private String stepDetectorText;
+    private String stepCounterText;
+    private String stepDetectorTimeText;
 
-    StepCountFunction(int aantalStappen)
+    private String isSupportStepDetector;
+    private String isSupportStepCounter;
+
+    private SensorManager sensorManager; // = een systeem service, gebruik voor om bepaalde sensor te krijgen
+
+    private Sensor stepCounter; //totaal steps sensoren
+    private Sensor stepDetector; //1 step sensoren
+
+    private SensorEventListener stepCounterListener;
+    private SensorEventListener stepDetectorListener;
+
+    private SimpleDateFormat simpleDateFormat; // gebruik om tijdstamp te bewaren
+
+    StepCountFunction(Context context)
     {
-        this.aantalStappen = aantalStappen;
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER); // stepCounter sensoor aan stepCount toekennen;
+        stepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR); //stepDetector sensoor
+
+        // packageManager is een manager van user's gsm,
+        // die bevat alle beschikbaar sensoren
+
+        isSupportStepCounter = String.valueOf(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER));
+        isSupportStepDetector = String.valueOf(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_DETECTOR));
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
 
     }
 
-    public static boolean isSupportStepCountSensor(Context context)
+    private void initListener()
     {
-        SensorManager sensorManager = (SensorManager) context.getSystemService(context.SENSOR_SERVICE);
+        stepCounterListener = new SensorEventListener() {
+            @Override   //Deze methode wordt geactiveerd wanneer de nauwkeurigheid van de geregistreerde sensor verandert.
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                Log.e("Counter-SensorChanged",sensorEvent.values[0]+"---"+sensorEvent.accuracy+"---"+sensorEvent.timestamp);
+                stepCounterText = ("Totaal steps" + sensorEvent.values[0]);
+            }
 
-        Sensor counterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        Sensor detectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            @Override //Deze methode wordt geactiveerd wanneer zich een nieuwe gebeurtenis voordoet op de geregistreerde sensor
+            public void onAccuracyChanged(Sensor sensor,int i) {
+                Log.e("Counter-Accuracy", sensor.getName()+"---"+i);
+            }
+        };
 
-        if (counterSensor != null || detectorSensor != null)
+        stepDetectorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                Log.e("Detector-SensorChanged",sensorEvent.values[0]+"---"+sensorEvent.accuracy+"---"+sensorEvent.timestamp);
+                stepDetectorText = "huidige stap count" + sensorEvent.values[0];
+                stepDetectorTimeText = "huidig staptijd" + simpleDateFormat.format(sensorEvent.timestamp/1000000);
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+                Log.e("Detector-Accuracy", sensor.getName()+"---"+i);
+            }
+        };
+
+    }
+
+    //Sensoor registeren betekent begint de sensor te gebruiken
+    //unregister = stop met het gebruiken van sensoren
+    private void  unregisterSensor(Context context){
+        if(context.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_SENSOR_STEP_COUNTER)
+                &&
+                context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_DETECTOR))
         {
-            return true;
+            sensorManager.unregisterListener(stepCounterListener);
+            sensorManager.unregisterListener(stepDetectorListener);
         }
-        return false;
     }
 
-    private boolean isBind = false;
-    private Messenger mGetReplyMessenger = new Messenger(new Handler(this));
-    private Messenger messenger;
-
-    //start count service
-    private void setupService(){
-        Intent intent = new Intent(this,StepService.class);
 
 
-    }
 
 }
