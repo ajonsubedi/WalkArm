a1ee0114cd900dd244b647c14412d5e174d3f193
Feng Lin <lin.feng55@hotmail.com>
2018-01-10 03:16:55 +0100

    StepCounter Done

--- app/src/main/java/com/example/ajonsubedi/walkarm/MainActivity.java
+++ app/src/main/java/com/example/ajonsubedi/walkarm/MainActivity.java
@@ -3,46 +3,34 @@ package com.example.ajonsubedi.walkarm;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
 
-//    Button btnNewAlarm;
+    Button btnNewAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.stepcount_test_page);
+        setContentView(R.layout.activity_main);
 
-//        btnNewAlarm = (Button)findViewById(R.id.btnNewAlarm);
-//
-//        btnNewAlarm.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//
-//        });
+        btnNewAlarm = (Button)findViewById(R.id.btnNewAlarm);
 
-        TextView isStepDetectorBeschikbaar = (TextView) findViewById(R.id.isStepDetectorBeschikbaar);
-        TextView isStepCounterBeschikbaar = (TextView) findViewById(R.id.isStepCounterBeschikbaar);
+        btnNewAlarm.setOnClickListener(new View.OnClickListener(){
 
-        TextView huidigStap = (TextView) findViewById(R.id.huidigStap);
-        TextView huidigTimeStap = (TextView) findViewById(R.id.huidigStapTime);
-        TextView totaalStap = (TextView) findViewById(R.id.totaalStap);
+            @Override
+            public void onClick(View v) {
 
-        StepCountFunction test = new StepCountFunction(this.getApplicationContext());
+            }
 
-        isStepCounterBeschikbaar.setText(test.isSupportStepCounter);
-        isStepDetectorBeschikbaar.setText(test.isSupportStepDetector);
-
-        test.initListener(totaalStap,huidigStap,huidigTimeStap);
-        test.registerSensor();
+        });
 
 
     }
--- app/src/main/java/com/example/ajonsubedi/walkarm/StepCounter.java
+++ app/src/main/java/com/example/ajonsubedi/walkarm/StepCounter.java
@@ -1,8 +1,96 @@
 package com.example.ajonsubedi.walkarm;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.widget.TextView;
+
 /**
  * Created by fenglin on 10/01/18.
  */
 
+/*
+!!! Stap 1:Moet eerst functie InitLinstener gebruiken
+    Stap 2: RegisterListener()
+    Stap 3: Reset(_
+ */
+
 public class StepCounter {
+
+    private int doelStappen;
+    private int huidigStappen;
+
+    private boolean heeftBereikt;
+    private SensorManager sensorManager; // = een systeem service, gebruik voor om bepaalde sensor te krijgen
+    private Sensor stepDetector; //deze sensor geeft 1 of 0 terug
+    private SensorEventListener stepDetectorListener;
+
+    // In mainActivity context = this.getApplicationContext()
+    StepCounter(int doelStappen, Context context) {
+
+        this.huidigStappen = 0;
+        this.doelStappen = doelStappen;
+        heeftBereikt = false;
+
+        this.sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        stepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+    }
+
+        public void InitListener(final TextView txtViewHuidigStappen)
+        {
+            stepDetectorListener = new SensorEventListener() {
+
+                @Override   //Deze methode wordt geactiveerd als de gebruiker stapt
+                public void onSensorChanged(SensorEvent sensorEvent) {
+
+                    huidigStappen++;    // Bij elke stap +1
+                    txtViewHuidigStappen.setText(Integer.toString(huidigStappen));
+                    HeeftDoelbreikt();
+
+                }
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int i) {
+
+                }
+            };
+        }
+
+    //Controleer of de doel heeft bereikt
+    private void HeeftDoelbreikt ()
+    {
+
+        if (huidigStappen == doelStappen)
+        {
+            heeftBereikt = true;
+            sensorManager.unregisterListener(stepDetectorListener);
+        }
+    }
+
+    //na voltooid van een wekker reset;
+    public void Reset()
+    {
+        huidigStappen = 0;
+        heeftBereikt = false;
+    }
+
+    public void RegisterListener()
+    {
+        sensorManager.registerListener(stepDetectorListener,stepDetector,sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public int GetHuidigStappen() {
+        return this.huidigStappen;
+    }
+
+    public boolean GetHeeftBereikt()
+    {
+        return heeftBereikt;
+    }
+
+
+
 }
