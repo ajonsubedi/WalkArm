92c75427ec2d300871b04351bcfcf2ccce384069
Feng Lin <lin.feng55@hotmail.com>
2018-01-10 04:00:13 +0100

    push voor ajon

--- app/src/main/java/com/example/ajonsubedi/walkarm/MainActivity.java
+++ app/src/main/java/com/example/ajonsubedi/walkarm/MainActivity.java
@@ -1,37 +1,62 @@
 package com.example.ajonsubedi.walkarm;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+
 
 import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnNewAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnNewAlarm = (Button)findViewById(R.id.btnNewAlarm);
+        String alarmName = getIntent().getStringExtra("name");
+        String alarmTime = getIntent().getStringExtra("time");
+        String alarmrepeat = getIntent().getStringExtra("repeat");
+        String alarmSteps = getIntent().getStringExtra("steps");
+        boolean alarmSwicth = getIntent().getBooleanExtra("switch", true);
 
-        btnNewAlarm.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
+        //ADD NEW ALARM
+        addNewAlarm();
 
-            }
-
-        });
-
+        //GET INFO FROM NEW ALARM
+        //SET ALARM TIME FROM SETUP
+        Intent i = getIntent();
+        String strAlarmTime = i.getStringExtra("alarmTime");
+        TextView setAlarmTime = (TextView)findViewById(R.id.textView);
+        setAlarmTime.setText(strAlarmTime);
 
     }
+
+
+        Button btnNewAlarm;
+        public void addNewAlarm(){
+            btnNewAlarm = (Button)findViewById(R.id.btnNewAlarm);
+            btnNewAlarm.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, setAlarm.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/ajonsubedi/walkarm/MyAlarmAdapter.java
+++ app/src/main/java/com/example/ajonsubedi/walkarm/MyAlarmAdapter.java
@@ -1,20 +1,42 @@
 package com.example.ajonsubedi.walkarm;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Switch;
+import android.widget.TextView;
 
 /**
- * Created by Ajon Subedi on 1/8/2018.
+ * Created by Ajon Subedi on 1/10/2018.
  */
 
-public class MyAlarmAdapter extends BroadcastReceiver {
-
+public class MyAlarmAdapter extends ArrayAdapter<String> {
+    public MyAlarmAdapter(Context context, String[] alarmInfos) {super(context,R.layout.my_alarm_item, alarmInfos);}
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.my_alarm_item, null);
 
+        //Get all content from my_alarm_item.xml
+        String listItem = getItem(position);
+        TextView alarmName = (TextView)view.findViewById(R.id.my_alarm_name_item);
+        TextView alarmTime = (TextView)view.findViewById(R.id.my_alarm_time_item);
+        TextView alarmRepeat = (TextView)view.findViewById(R.id.my_alarm_repeat_item);
+        TextView alarmSteps = (TextView)view.findViewById(R.id.my_alarm_steps_item);
+        Switch switchOnOffAlarm = (Switch)view.findViewById(R.id.my_alarm_switchButton_item);
+
+        alarmName.setText(listItem);
+        alarmTime.setText(listItem);
+        alarmRepeat.setText(listItem);
+        alarmSteps.setText(listItem);
+        switchOnOffAlarm.setChecked(isEnabled(position));
+
+        return view;
     }
 }
+
+
